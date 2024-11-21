a327760fa64010f0de0522c71f7ec1ac6196aeb9
Van-Fim <Vanoefim@gmail.com>
2024-11-21 16:07:45 +0300

    Deleted netcode

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;
 using System;
 using Unity.VisualScripting;
 public class GameManager : MonoBehaviour
--- Assets/Scripts/SpaceObject/SpaceObject.cs
+++ Assets/Scripts/SpaceObject/SpaceObject.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Data;
 using Mirror;
-using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
 
